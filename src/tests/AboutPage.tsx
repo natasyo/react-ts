8c48f175ff442fae5cfbc550509c4f40079a6518
natasyo <natasyo23@gmail.com>
2023-03-19 20:36:15 +0300

    feat:tests

--- src/tests/AboutPage.tsx
+++ src/tests/AboutPage.tsx
@@ -7,6 +7,6 @@ describe('About page', () => {
   it('render', () => {
     render(<AboutPage />);
     expect(screen.getByPlaceholderText('Search')).toBeInTheDocument();
-    expect(screen.getByText('About')).toBeInTheDocument()
+    expect(screen.getByText('About')).toBeInTheDocument();
   });
 });
--- src/tests/ProductsComponent.test.tsx
+++ src/tests/ProductsComponent.test.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { describe, expect, it } from 'vitest';
 
 import { render, screen } from '@testing-library/react';
-import * as data from '../data/products.json';
-import { IProduct } from '../types/iProduct';
 import { ProductsComponent } from '../components/ProductComponent/ProductsComponent';
 describe('Products component', () => {
   it('render', () => {
